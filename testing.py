f05f1291ed0542965cff46226766e88cb2d8fb34
allx4461 <137797438+allx4461@users.noreply.github.com>
2025-11-24 23:24:12 +0300

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,40 +1,43 @@
 import generators as gn
 import search as sc
 import matplotlib.pyplot as plt
+import numpy as np
 import time
 timeline=[]
 res1=[]
 res2=[]
 res3=[]
 res4=[]
-res5=[]
-time_binpoisk=[]
-time_pointers=[]
-time_exponent=[]
 time_binrazdel=[]
-for l in range(100,5000,10):
-    [m1,m2]=gn.generate_fortest(l)
+r=1
+k=5
+for l in range(10,1000,1):
+    [m1,m2]=gn.generate_fortest(l,r)
     timeline.append(l)
     time_1=time.time()
-    sc.two_pointers(m1,m2)
+    for i in range(k):sc.two_pointers(m1,m2)
     time_2=time.time()
-    sc.bin_perebor(m1,m2,0,l-1)
+    for i in range(k):sc.start_bin_perebor(m1,m2)
     time_3=time.time()
-    sc.exppoisk(m1,m2)
+    for i in range(k):sc.exppoisk(m1,m2)
     time_4=time.time()
-    sc.binsmallermassive(m1,m2)
+    for i in range(k):sc.binsmallermassive(m1,m2)
     time_5=time.time()
 
-    res1.append((time_2-time_1)*100)
-    res2.append((time_3 - time_2)*100)
-    res3.append((time_4 - time_3)*100)
-    res4.append((time_5 - time_4)*100)
+    res1.append((time_2-time_1))
+    res2.append((time_3 - time_2))
+    res3.append((time_4 - time_3))
+    res4.append((time_5 - time_4))
 
 
 fig, ax = plt.subplots()
-plt.title('четные и нечетные')
+plt.title('четные и нечетные (размер отличается в 1000 раз)')
 plt.xlabel('size')
 plt.ylabel('time')
+plt.ylim([10**-5, 10**-2])
+plt.xscale('log')
+plt.yscale('log')
+plt.tight_layout()
 ax.plot(timeline, res1, label="two_pointers")
 ax.plot(timeline, res2, label="bin_split")
 ax.plot(timeline, res3, label="exppoisk")
